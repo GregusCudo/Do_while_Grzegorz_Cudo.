3bfa71290287fe7f2ef98a8fa7a2a0b1b2e308e4
GregusCudo <120098192+GregusCudo@users.noreply.github.com>
2023-06-16 23:39:53 +0200

    Add files via upload

---
+++ untitled/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ile=5;
+    do
+    {
+        cout <<"petla kolejna"<<endl;
+        cout <<"petla kolejna"<<endl;
+        cout <<"petla kolejna"<<endl;
+        cout <<"petla kolejna"<<endl;
+    }
+    while (ile<19);
+     cout <<endl<<" Nacisnij ENTER aby zakonczyc"<<endl;
+    getchar();
+    return 0;
+}
